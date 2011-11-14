b9581243c202bed425e0290392de3a966557e448
Alex Chap <alexandre.chappuis@epfl.ch>
2011-11-14 15:33:41 +0100

    First topology test scripts

---
+++ testing/conf/top01/reset.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# Reset configuration to default
+
+echo "Kill Bird and virtual switch"
+sudo pkill bird
+sudo pkill vde_switch
+
+echo "Remove tap interfaces"
+sudo ip link set tap11 down
+sudo ip link set tap12 down
+sudo openvpn --rmtun --dev tap11
+sudo openvpn --rmtun --dev tap12
