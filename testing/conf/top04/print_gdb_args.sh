cda1dfe45c1f02af769bd17728589ed406835a5f
Alex Chap <alexandre.chappuis@epfl.ch>
2011-12-09 16:56:47 +0100

    Add useful scripts

---
+++ testing/conf/top04/print_gdb_args.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+echo "run -c `pwd`/rta.conf -s /usr/local/var/run/bird_rta.ctl -D /var/log/bird-top04-rta.debug"
---
+++ testing/conf/top04/start_updates.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+ # Start packet generator 
+ echo "Starting packet generator"
+ sudo ../../tools/mdfmt/update_regenerator/update_regenerator.py -m ../../updates/updates.20100401.1729 -d 10.10.10.11 -a ./ASIP
+
