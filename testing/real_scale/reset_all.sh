157a79d9c61b4e10ca25fbdc868ad3b3be428dcd
Alex Chap <alexandre.chappuis@epfl.ch>
2011-12-14 14:38:56 +0100

    Scripts for real scale testing 1 working

--- testing/real_scale/reset_all.sh
+++ testing/real_scale/reset_all.sh
@@ -2,7 +2,11 @@
 
 # This script kills all bird instances and delete all 
 # log and debugging files
+echo "Kill daemons"
 pkill bird
+pkill python
+pkill -9 start_feeder.sh 
+echo "Del logs"
 rm ./log-dbg/*.log
 rm ./log-dbg/*.debug
 
